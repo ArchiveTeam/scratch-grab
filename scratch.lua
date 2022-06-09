58e895262fd8b308c1cbe4d8ca2064549965acf8
arkiver <arkiver@protonmail.com>
2022-06-09 17:13:26 +0200

    initial

---
+++ scratch.lua
@@ -0,0 +1,432 @@
+dofile("table_show.lua")
+dofile("urlcode.lua")
+local urlparse = require("socket.url")
+local http = require("socket.http")
+JSON = (loadfile "JSON.lua")()
+
+local item_dir = os.getenv("item_dir")
+local warc_file_base = os.getenv("warc_file_base")
+local item_type = nil
+local item_name = nil
+local item_value = nil
+
+if urlparse == nil or http == nil then
+  io.stdout:write("socket not corrently installed.\n")
+  io.stdout:flush()
+  abortgrab = true
+end
+
+local url_count = 0
+local tries = 0
+local downloaded = {}
+local addedtolist = {}
+local abortgrab = false
+local killgrab = false
+local queue_resources = true
+
+local username = nil
+
+local md5_hash = ""
+for i=1,32 do
+  md5_hash = md5_hash .. "[0-9a-f]"
+end
+md5_hash = '"(' .. md5_hash .. '%.[^"]+)"'
+
+local discovered_outlinks = {}
+local discovered_items = {}
+local bad_items = {}
+local ids = {}
+
+local retry_url = false
+
+for ignore in io.open("ignore-list", "r"):lines() do
+  downloaded[ignore] = true
+end
+
+abort_item = function(item)
+  abortgrab = true
+  if not item then
+    item = item_name
+  end
+  if not bad_items[item] then
+    io.stdout:write("Aborting item " .. item .. ".\n")
+    io.stdout:flush()
+    bad_items[item] = true
+  end
+end
+
+kill_grab = function(item)
+  io.stdout:write("Aborting crawling.\n")
+  killgrab = true
+end
+
+read_file = function(file)
+  if file then
+    local f = assert(io.open(file))
+    local data = f:read("*all")
+    f:close()
+    return data
+  else
+    return ""
+  end
+end
+
+processed = function(url)
+  if downloaded[url] or addedtolist[url] then
+    return true
+  end
+  return false
+end
+
+discover_item = function(target, item)
+  if not target[item] then
+    target[item] = true
+  end
+end
+
+find_item = function(url)
+  value = string.match(url, "^https?://projects.scratch.mit.edu/([0-9]+)$")
+  type_ = "p"
+  if not value then
+    value = string.match(url, "^https?://assets%.scratch%.mit%.edu/internalapi/asset/([^/]+)/get/$")
+    type_ = "a"
+  end
+  if value then
+    item_type = type_
+    item_value = value
+    ids = {}
+    if type_ == "a" then
+      ids[string.match(value, "^([a-f0-9]+)")] = true
+    else
+      ids[value] = true
+    end
+    item_name_new = item_type .. ":" .. item_value
+    if item_name_new ~= item_name then
+      abortgrab = false
+      tries = 0
+      item_name = item_name_new
+      username = nil
+      print("Archiving item " .. item_name)
+    end
+  end
+end
+
+allowed = function(url, parenturl)
+  for s in string.gmatch(url, "([a-f0-9]+)") do
+    if ids[s] then
+      return true
+    end
+  end
+
+  return false
+end
+
+wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_parsed, iri, verdict, reason)
+  local url = urlpos["url"]["url"]
+  local html = urlpos["link_expect_html"]
+  
+  return false
+end
+
+wget.callbacks.get_urls = function(file, url, is_css, iri)
+  local urls = {}
+  local html = nil
+  
+  downloaded[url] = true
+
+  if abortgrab then
+    return {}
+  end
+
+  if item_type == "url" then
+    return urls
+  end
+
+  local function decode_codepoint(newurl)
+    newurl = string.gsub(
+      newurl, "\\[uU]([0-9a-fA-F][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F])",
+      function (s)
+        return unicode_codepoint_as_utf8(tonumber(s, 16))
+      end
+    )
+    return newurl
+  end
+
+  local function check(newurl)
+    newurl = decode_codepoint(newurl)
+    local origurl = url
+    local url = string.match(newurl, "^([^#]+)")
+    local url_ = string.match(url, "^(.-)[%.\\]*$")
+    while string.find(url_, "&amp;") do
+      url_ = string.gsub(url_, "&amp;", "&")
+    end
+    if not processed(url_)
+      and string.match(url_, "^https?://[^/%.]+%..+")
+      and allowed(url_, origurl) then
+      if string.match(url_, "%?before=") or string.match(url_, "%?after=") then
+        table.insert(urls, {
+          url=url_,
+          headers={
+            ["X-Requested-With"]="XMLHttpRequest",
+            ["Accept"]="application/json, text/javascript, */*; q=0.01"
+          }
+        })
+      else
+        table.insert(urls, { url=url_ })
+      end
+      addedtolist[url_] = true
+      addedtolist[url] = true
+    end
+  end
+
+  local function checknewurl(newurl)
+    newurl = decode_codepoint(newurl)
+    if string.match(newurl, "['\"><]") then
+      return nil
+    end
+    if string.match(newurl, "^https?:////") then
+      check(string.gsub(newurl, ":////", "://"))
+    elseif string.match(newurl, "^https?://") then
+      check(newurl)
+    elseif string.match(newurl, "^https?:\\/\\?/") then
+      check(string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^\\/\\/") then
+      checknewurl(string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^//") then
+      check(urlparse.absolute(url, newurl))
+    elseif string.match(newurl, "^\\/") then
+      checknewurl(string.gsub(newurl, "\\", ""))
+    elseif string.match(newurl, "^/") then
+      check(urlparse.absolute(url, newurl))
+    elseif string.match(newurl, "^%.%./") then
+      if string.match(url, "^https?://[^/]+/[^/]+/") then
+        check(urlparse.absolute(url, newurl))
+      else
+        checknewurl(string.match(newurl, "^%.%.(/.+)$"))
+      end
+    elseif string.match(newurl, "^%./") then
+      check(urlparse.absolute(url, newurl))
+    end
+  end
+
+  local function checknewshorturl(newurl)
+    newurl = decode_codepoint(newurl)
+    if string.match(newurl, "^%?") then
+      check(urlparse.absolute(url, newurl))
+    elseif not (
+      string.match(newurl, "^https?:\\?/\\?//?/?")
+      or string.match(newurl, "^[/\\]")
+      or string.match(newurl, "^%./")
+      or string.match(newurl, "^[jJ]ava[sS]cript:")
+      or string.match(newurl, "^[mM]ail[tT]o:")
+      or string.match(newurl, "^vine:")
+      or string.match(newurl, "^android%-app:")
+      or string.match(newurl, "^ios%-app:")
+      or string.match(newurl, "^data:")
+      or string.match(newurl, "^irc:")
+      or string.match(newurl, "^%${")
+    ) then
+      check(urlparse.absolute(url, newurl))
+    end
+  end
+
+  local function queue_assets(data)
+    local data_format = data["dataFormat"]
+    local asset_id = data["assetId"]
+    if data_format and asset_id then
+      discover_item(discovered_items, "a:" .. asset_id .. "." .. data_format)
+    end
+    for k, v in pairs(data) do
+      if type(v) == "table" then
+        queue_assets(v)
+      end
+    end
+  end
+
+  if allowed(url) and status_code < 300 then
+    html = read_file(file)
+    if string.match(url, "^https?://projects%.scratch%.mit%.edu/") then
+      check("https://uploads.scratch.mit.edu/get_image/project/" .. item_value .. "_480x360.png")
+      check("https://uploads.scratch.mit.edu/projects/thumbnails/" .. item_value .. ".png")
+      check("https://scratch.mit.edu/projects/" .. item_value .. "/")
+      check("https://api.scratch.mit.edu/projects/" .. item_value)
+      for s in string.gmatch(html, md5_hash) do
+        discover_item(discovered_items, "a:" .. s)
+      end
+      queue_assets(JSON:decode(html))
+    end
+    if string.match(url, "^https?://api%.scratch%.mit%.edu/projects/[0-9]+$") then
+      local json = JSON:decode(html)
+      username = json["author"]["username"]
+      check("https://api.scratch.mit.edu/projects/" .. item_value .. "/remixes?limit=5")
+      check("https://api.scratch.mit.edu/users/" .. username .. "/projects/" .. item_value .. "/studios")
+      check("https://api.scratch.mit.edu/users/" .. username .. "/projects/" .. item_value .. "/comments?offset=0&limit=20")
+    end
+    if string.match(url, "/comments%?") or string.match(url, "/replies%?") then
+      local json = JSON:decode(html)
+      local count = 0
+      local is_comment = string.match(url, "/comments%?")
+      for _, d in pairs(json) do
+        count = count + 1
+        if is_comment then
+          if d["reply_count"] > 0 then
+            check("https://api.scratch.mit.edu/users/" .. username .. "/projects/" .. item_value .. "/comments/" .. d["id"] .. "/replies?offset=0&limit=25")
+          end
+        end
+      end
+      local limit = tonumber(string.match(url, "[%?&]limit=([0-9]+)"))
+      if count == limit then
+        local offset = tonumber(string.match(url, "[%?&]offset=([0-9]+)"))
+        check(string.gsub(url, "([%?&]offset=)[0-9]+", "%1" .. tostring(offset+limit)))
+      end
+    end
+    --[[for newurl in string.gmatch(string.gsub(html, "&quot;", '"'), '([^"]+)') do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(string.gsub(html, "&#039;", "'"), "([^']+)") do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(html, ">%s*([^<%s]+)") do
+      checknewurl(newurl)
+    end
+    for newurl in string.gmatch(html, "[^%-]href='([^']+)'") do
+      checknewshorturl(newurl)
+    end
+    for newurl in string.gmatch(html, '[^%-]href="([^"]+)"') do
+      checknewshorturl(newurl)
+    end
+    for newurl in string.gmatch(html, ":%s*url%(([^%)]+)%)") do
+      checknewurl(newurl)
+    end]]
+  end
+
+  return urls
+end
+
+wget.callbacks.write_to_warc = function(url, http_stat)
+  find_item(url["url"])
+  return true
+end
+
+wget.callbacks.httploop_result = function(url, err, http_stat)
+  status_code = http_stat["statcode"]
+  
+  url_count = url_count + 1
+  io.stdout:write(url_count .. "=" .. status_code .. " " .. url["url"] .. " \n")
+  io.stdout:flush()
+
+  if killgrab then
+    return wget.actions.ABORT
+  end
+
+  find_item(url["url"])
+
+  if status_code >= 300 and status_code <= 399 then
+    local newloc = urlparse.absolute(url["url"], http_stat["newloc"])
+    if processed(newloc) or not allowed(newloc, url["url"]) then
+      tries = 0
+      return wget.actions.EXIT
+    end
+  end
+  
+  if status_code == 200 then
+    downloaded[url["url"]] = true
+    downloaded[string.gsub(url["url"], "https?://", "http://")] = true
+  end
+
+  if abortgrab then
+    abort_item()
+    return wget.actions.EXIT
+  end
+
+  if status_code ~= 200 and status_code ~= 404 then
+    io.stdout:write("Server returned bad response. Sleeping.\n")
+    io.stdout:flush()
+    local maxtries = 10
+    tries = tries + 1
+    if tries > maxtries then
+      tries = 0
+      abort_item()
+      return wget.actions.ABORT
+    end
+    os.execute("sleep " .. math.random(
+      math.floor(math.pow(2, tries-0.5)),
+      math.floor(math.pow(2, tries))
+    ))
+    return wget.actions.CONTINUE
+  end
+
+  tries = 0
+
+  return wget.actions.NOTHING
+end
+
+wget.callbacks.finish = function(start_time, end_time, wall_time, numurls, total_downloaded_bytes, total_download_time)
+  local function submit_backfeed(items, key)
+    local tries = 0
+    local maxtries = 4
+    while tries < maxtries do
+      if killgrab then
+        return false
+      end
+      local body, code, headers, status = http.request(
+        "https://legacy-api.arpa.li/backfeed/legacy/" .. key,
+        items .. "\0"
+      )
+      if code == 200 and body ~= nil and JSON:decode(body)["status_code"] == 200 then
+        io.stdout:write(string.match(body, "^(.-)%s*$") .. "\n")
+        io.stdout:flush()
+        break
+      end
+      io.stdout:write("Failed to submit discovered URLs." .. tostring(code) .. tostring(body) .. "\n")
+      io.stdout:flush()
+      os.execute("sleep " .. math.floor(math.pow(2, tries)))
+      tries = tries + 1
+    end
+    if tries == maxtries then
+      kill_grab()
+    end
+  end
+
+  local file = io.open(item_dir .. "/" .. warc_file_base .. "_bad-items.txt", "w")
+  for url, _ in pairs(bad_items) do
+    file:write(url .. "\n")
+  end
+  file:close()
+  for key, data in pairs({
+    ["scratch-30ibp1crc3riaqx"] = discovered_items,
+    --[""] = discovered_outlinks
+  }) do
+    print('queuing for', string.match(key, "^(.+)%-"))
+    local items = nil
+    local count = 0
+    for item, _ in pairs(data) do
+      print("found item", item)
+      if items == nil then
+        items = item
+      else
+        items = items .. "\0" .. item
+      end
+      count = count + 1
+      if count == 100 then
+        submit_backfeed(items, key)
+        items = nil
+        count = 0
+      end
+    end
+    if items ~= nil then
+      submit_backfeed(items, key)
+    end
+  end
+end
+
+wget.callbacks.before_exit = function(exit_status, exit_status_string)
+  if killgrab then
+    return wget.exits.IO_FAIL
+  end
+  if abortgrab then
+    abort_item()
+  end
+  return exit_status
+end
+
